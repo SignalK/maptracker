9101928514093b95409db30fab5d05a647247c12
Teppo Kurki <tjk@iki.fi>
2015-11-03 20:26:28 +0200

    Replace Gulp with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    map: './map.js'
+  },
+  output: {
+    path: 'dist',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.json$/, loader: 'json'}
+    ]
+  },
+  plugins: [
+  ]
+}
