2680585c071924907b36bed0b49e31aeb52fef88
Teppo Kurki <tjk@iki.fi>
2016-01-04 09:23:59 +0200

    Convert radians to degrees

--- map.js
+++ map.js
@@ -4,4 +4,6 @@ L.Icon.Default.imagePath = '/bower_components/leaflet/dist/images/';
 d3 = require('d3');
 $ = require('jquery');
 
-signalk = require('signalk-client')
+signalk = require('signalk-client');
+
+Qty = require('js-quantities');
